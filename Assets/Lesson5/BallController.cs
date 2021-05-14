379f58d0e03bb877070b0672d30b416b3506bc1f
michiya-noro <81213169+michiya-noro@users.noreply.github.com>
2021-05-14 21:55:50 +0900

    課題

--- Assets/Lesson5/BallController.cs
+++ Assets/Lesson5/BallController.cs
@@ -8,25 +8,49 @@ public class BallController : MonoBehaviour
 
         //ボールが見える可能性のあるz軸の最大値
         private float visiblePosZ = -6.5f;
-
+        private int Point = 0;
+        
         //ゲームオーバを表示するテキスト
         private GameObject gameoverText;
-
+        private GameObject PointText;
         // Use this for initialization
         void Start ()
         {
                 //シーン中のGameOverTextオブジェクトを取得
                 this.gameoverText = GameObject.Find("GameOverText");
+                     PointText = GameObject.Find("PointText");
         }
 
+    
         // Update is called once per frame
-        void Update ()
+        void Update()
         {
-                //ボールが画面外に出た場合
-                if (this.transform.position.z < this.visiblePosZ)
-                {
-                        //GameoverTextにゲームオーバを表示
-                        this.gameoverText.GetComponent<Text> ().text = "Game Over";
-                }
+            //ボールが画面外に出た場合
+            if (this.transform.position.z < this.visiblePosZ)
+            {
+                //GameoverTextにゲームオーバを表示
+                this.gameoverText.GetComponent<Text>().text = "GameOver";
+            }
+            PointText.GetComponent<Text>().text = Point + "Point";
+            Debug.Log(Point);
+        }
+        void OnCollisionEnter(Collision other)
+        {
+           if (other.gameObject.tag == "SmallStarTag")
+            {
+            this.Point += 1;
+             }
+            else if (other.gameObject.tag == "LargeStarTag")
+             {
+            this.Point += 5;
+             }
+             else if (other.gameObject.tag == "SmallCloudTag")
+             {
+            this.Point += 3;
+              }
+             else if (other.gameObject.tag == "LargeCloudTag")
+              {
+            this.Point += 10;
+              }
         }
 }
